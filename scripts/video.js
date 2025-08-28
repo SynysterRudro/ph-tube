08573ee51e1147e662b59efb131c2c22d14b15be
Rudro <user@Rudro-Avenger.local>
2025-08-28 12:55:16 +0600

    video section added successfully

--- scripts/video.js
+++ scripts/video.js
@@ -12,6 +12,74 @@ const loadCategories = () => {
         .catch(error => console.log(error));
 }
 
+
+// loading videos 
+
+const loadVideos = () => {
+
+    console.log("load categories connected");
+    // fetch the data
+    fetch(' https://openapi.programming-hero.com/api/phero-tube/videos')
+        .then(res => res.json())
+        .then(data => displayVideos(data.videos))
+        .catch(error => console.log(error));
+}
+
+
+// display videos 
+
+const displayVideos = (videos) => {
+    // console.log(videos);
+
+    const videoContainer = document.getElementById('videos');
+
+    videos.forEach(video => {
+        console.log(video);
+
+        const card = document.createElement('div');
+        card.classList = "card card-compact";
+        card.innerHTML = `
+        <figure class="h-[200px]">
+    <img
+      src="${video.thumbnail}"
+      alt="Shoes"
+      class="rounded-xl h-full w-full object-cover" />
+  </figure>
+  <div class="px-0 py-2 flex gap-2">
+   
+<div>
+
+<img
+      src="${video.authors[0].profile_picture}"
+      alt="profile picture"
+      class="w-10 h-10 rounded-full object-cover" />
+
+</div>
+<div class="px-2 py-2">
+  <!-- Video Title -->
+  <h2 class="font-bold">${video.title}</h2>
+
+  <!-- Author + Verified Badge -->
+  <div class="flex items-center gap-2 mt-1">
+    <p class="text-gray-400">${video.authors[0].profile_name}</p>
+    <img
+      src="https://img.icons8.com/?size=100&id=98A4yZTt9abw&format=png&color=000000"
+      alt="verified"
+      class="w-5"
+    />
+  </div>
+
+  <!-- fh text -->
+  <p>fh</p>
+</div>
+
+
+  </div>
+        `;
+        videoContainer.append(card)
+    })
+}
+
 // display loaded categories
 
 const displayCategories = (categories) => {
@@ -33,3 +101,4 @@ const displayCategories = (categories) => {
 }
 
 loadCategories();
+loadVideos();
